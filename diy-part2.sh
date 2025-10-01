e3d03a2ff3d204f47d95c075f1c74f67a93c6326
Beanbag7 <56281910+Beanbag7@users.noreply.github.com>
2025-10-01 10:55:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,4 +10,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
-git clone https://github.com/RuijieNetworksCommunity/feed-netkeeper.git
+# git clone https://github.com/RuijieNetworksCommunity/feed-netkeeper.git
